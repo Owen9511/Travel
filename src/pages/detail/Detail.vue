c844ca34b0cc31b90409f6b05e37aaccb84eb2df
张柯雨 <zhangkeyu@ZBMAC-VX5ZDHTD5.local>
2019-03-14 17:59:05 +0800

    add detail ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <detail-banner/>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :gallaryImgs="gallaryImgs"/>
     <detail-header/>
     <div class="content">
       <detail-list :list="list"/>
@@ -12,6 +15,7 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
@@ -19,25 +23,35 @@ export default {
     DetailHeader,
     DetailList
   },
+  mounted () {
+    this.getDetailInfo()
+  },
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三馆联票',
-          children: [{
-            title: '成人三馆联票-某店'
-          }]
-        }, {
-          title: '成人五馆联票'
-        }]
-      }, {
-        title: '学生票'
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: [],
+      bannerInfo: {}
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.list = data.categoryList
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+      }
     }
   }
 }
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -5,17 +5,17 @@
       @click="handleBannerClick">
       <img
         class="banner-img"
-        src="//img1.qunarzz.com/sight/p0/1412/77/cb5a5c0c47e96f6b42ce646cddf4af0d.water.jpg_600x330_17d4b9aa.jpg"/>
+        :src="bannerImg"/>
       <div class="banner-info">
-        <div class="banner-title">鸟巢(AAAAA景区)</div>
+        <div class="banner-title">{{sightName}}</div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe6f5;</span>
-          138
+          {{gallaryImgs.length}}
         </div>
       </div>
     </div>
     <common-gallery
-      :imgs="imgs"
+      :imgs="gallaryImgs"
       v-if="showGallary"
       @close="closeGallary"/>
   </div>
@@ -28,11 +28,14 @@ export default {
   components: {
     CommonGallery
   },
+  props: {
+    sightName: String,
+    bannerImg: String,
+    gallaryImgs: Array
+  },
   data () {
     return {
-      showGallary: false,
-      imgs: ['http://img1.qunarzz.com/sight/p0/1412/f7/73c43024f19e33c695b364ccea1db63b.water.jpg_r_800x800_9cc7ad8b.jpg',
-        'http://img1.qunarzz.com/sight/p0/1507/c9/eefaa23facb37ac4814e336070207804.water.jpg_r_800x800_c58340e6.jpg']
+      showGallary: false
     }
   },
   activated () {
